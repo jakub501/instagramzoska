17ecd875bccd3903aba2d02bec67f2eebe0db96c
jakub501 <jakub.kockovsky@gmail.com>
2024-12-04 13:26:58 +0100

    upravy

---
+++ prisma/migrations/20241127133026_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "caption" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,38 @@
+// src/components/NavBar.tsx
+
+"use client";
+
+import * as React from 'react';
+import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import InfoIcon from '@mui/icons-material/Info';
+//import GavelIcon from '@mui/icons-material/Gavel';
+
+import { useRouter } from 'next/navigation';
+
+export default function NavBar() {
+  const [value, setValue] = React.useState('/');
+  const router = useRouter();
+
+  const handleNavigation = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+    router.push(newValue); 
+  };
+
+  return (
+    <Box sx={{ width: '100%', position: 'fixed', bottom: 0 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={handleNavigation}
+      >
+        <BottomNavigationAction label="Domov" value="/" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Profily" value="/profil" icon={<AccountCircleIcon />} />
+        <BottomNavigationAction label="Príspevky" value="/prispevok" icon={<AddCircleIcon />} />
+        <BottomNavigationAction label="O nás" value="/o-nas" icon={<InfoIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
